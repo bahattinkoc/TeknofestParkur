a81746776c702a5b1b72981da772b9c440376b6c
Bahattin <bahattink3458@gmail.com>
2021-03-18 02:31:43 +0300

    adding trafic light script

---
+++ Scripts/TrafficLightSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrafficLightSwitcher : MonoBehaviour
+{
+    public Material redLight, yellowLight, greenLight;
+    public GameObject obj1, obj2, obj3;
+    int counter = 0;
+
+    void Start()
+    {
+        obj1.GetComponent<MeshRenderer>().material = redLight;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            switch (counter)
+            {
+                case 0:
+                    MaterialChanger(yellowLight); break;
+
+                case 1:
+                    MaterialChanger(greenLight); break;
+
+                case 2:
+                    MaterialChanger(redLight); break;
+            }
+            counter = ((counter + 1) > 2) ? 0 : counter + 1;
+        }
+    }
+
+    void MaterialChanger(Material mat)
+    {
+        obj1.GetComponent<MeshRenderer>().material = obj2.GetComponent<MeshRenderer>().material = obj3.GetComponent<MeshRenderer>().material = mat;
+    }
+}
